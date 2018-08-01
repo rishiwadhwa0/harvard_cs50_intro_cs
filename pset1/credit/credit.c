701f3b35dc363f3d9358a4838a548db8608f08b1
Rishi Wadhwa <rishiw2@illinois.edu>
2018-08-01 05:28:07 +0000

    adding credit program

---
+++ pset1/credit/credit.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+//checks whether a credit card number is valid and prints what company the credit card is associated with
+
+int llong_len();
+int int_len();
+string check_starting_numbs();
+
+int main(void)
+{
+    int length = 0;
+    long long ccnum = 0;
+
+    ccnum = get_long_long("What is you're credit card number?: ");
+    length = llong_len(ccnum);
+
+    if (length != 13 && length != 15 && length != 16)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    int digits[length];
+
+    for (int i = length - 1; i >= 0; i--)
+    {
+        digits[i] = ccnum % 10;
+        ccnum = floor(ccnum / 10);
+    }
+
+    /*
+    for (int i = 0; i < length; i++)
+    {
+        printf("%i", digits[i]);
+    }
+
+    printf("\n");
+    */
+
+    string company = check_starting_numbs(digits);
+
+    if (strcmp(company, "INVALID") == 0)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    int total_under = 0;
+    int int_length = 0;
+    for (int i = length - 2; i >= 0; i = i - 2)
+    {
+        digits[i] = digits[i] * 2;
+        int_length = int_len(digits[i]);
+
+        int a[length];
+        int num = digits[i];
+        for (int b = int_length - 1; b >= 0; b--)
+        {
+            a[b] = num % 10;
+            total_under = total_under + a[b];
+            num = num / 10;
+        }
+    }
+
+    int total_other = 0;
+    for (int i = length - 1; i >= 0; i = i - 2)
+    {
+        total_other = total_other + digits[i];
+
+    }
+
+    int total = total_under + total_other;
+
+    if (total % 10 == 0)
+    {
+        printf("%s\n", company);
+    }
+
+    else
+        printf("INVALID\n");
+
+}
+
+
+int llong_len(long long n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+
+    return floor(log10(llabs(n))) + 1;
+}
+
+
+int int_len(int n)
+{
+     if(n == 0)
+    {
+        return 0;
+    }
+
+    return floor(log10(abs(n))) + 1;
+}
+
+
+string check_starting_numbs(int a[])
+{
+    if (a[0] == 4)
+    {
+        return "VISA";
+    }
+
+    if ( (a[0] == 3 && a[1] == 4) || (a[0] == 3 && a[1] == 7) )
+    {
+        return "AMEX";
+    }
+
+    if (  a[0] == 5 && ( (a[1] >= 1) && (a[1] <= 5) )  )
+    {
+        return "MASTERCARD";
+    }
+
+    return "INVALID";
+}
